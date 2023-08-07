17540d3d8643fe83f5512d485ffd6b05eed5fafa
bradenasmith2 <bradenasmith@icloud.com>
2023-08-07 16:47:08 -0500

    Statistics Kickoff: created controller, view, and added a link on the nav bar. NO DATA in or out.

---
+++ MvcMessageLogger/Controllers/StatisticsConrtroller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMessageLogger.DataAccess;
+
+namespace MvcMessageLogger.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly MvcMessageLoggerContext _context;
+
+        public StatisticsController(MvcMessageLoggerContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
